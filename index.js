958889cae1e12f528ed51924d40222354b8042f8
Re595 <renato_galindo@outlook.com>
2022-07-13 17:45:44 -0400

    Update to 1.0.1 Added new route ubication (load the closest ubication to the seached term) Added Datalist for closest ubication on Index.html Added throw error to scrape to web function

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 const path = require('path');
 const { toWeb }  = require("./scrape");
 var app = express();
+
+const axios = require('axios');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -11,6 +14,29 @@ app.use("/home", express.static(__dirname + "/home"));
 app.use("/css", express.static(__dirname + "/home/css"));
 app.use("/js", express.static(__dirname + "/home/js"));
 /* Ruta get :query, es la principal, ver el index.html para ver ejemplo de consumo a esta ruta*/
+app.get('/ubication/', async (req,res)=>{
+    let { query } = req;  
+    console.log(query.country); 
+    try{
+        let headersList = {
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"  
+           }
+           
+           let reqOptions = {
+             url: `https://www.abctelefonos.com/ubicacion?=country=${query.country}&term=${query.term}`,
+             method: "GET",
+             headers: headersList,
+             responseEncoding: 'latin1',
+           }
+           const response = await axios.request(reqOptions); 
+           let { data } = response;  
+           return await res.send(data);
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({ error: error });
+    }
+}); 
 app.get('/:query/', (req,res)=>{
     let { url } = req; 
     try{
